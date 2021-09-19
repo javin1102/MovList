04b0411af13da38ed441c6bdc1e9cab970155acd
javin1102 <javinrio112@gmail.com>
2021-09-19 16:46:06 +0700

    Fixing bug on schema.js

--- server/schema.js
+++ server/schema.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+
 const {
   GraphQLObjectType,
   GraphQLInt,
@@ -9,7 +10,7 @@ const {
 const YOUR_API = process.env.API_KEY;
 
 const NewMoviesType = new GraphQLObjectType({
-  name: "New Movies",
+  name: "NewMovies",
   fields: {
     id: { type: GraphQLInt },
     title: { type: GraphQLString },
@@ -17,37 +18,42 @@ const NewMoviesType = new GraphQLObjectType({
   },
 });
 
-const MovieQueryType = new GraphQLObjectType({
-  name: "MovieQuery",
+const RootQueryType = new GraphQLObjectType({
+  name: "RootQuery",
   fields: {
-    newMovies: new GraphQLList(NewMoviesType),
-    resolve() {
-      return axios
-        .get(
-          `https://api.themoviedb.org/3/movie/now_playing?api_key=${YOUR_API}&language=en-US&page=1`
-        )
-        .then((res) => {
-          const movies = res.data.results;
-          movies.map(
-            (movie) =>
-              (movie.poster_path =
-                "https://image.tmdb.org/t/p/w500" + movie.poster_path)
-          );
-          return movies;
-        });
+    newMovies: {
+      type: new GraphQLList(NewMoviesType),
+      resolve() {
+        return axios
+          .get(
+            `https://api.themoviedb.org/3/movie/now_playing?api_key=${YOUR_API}&language=en-US&page=1`
+          )
+          .then((res) => {
+            const movies = res.data.results;
+            movies.map(
+              (movie) =>
+                (movie.poster_path =
+                  "https://image.tmdb.org/t/p/w500" + movie.poster_path)
+            );
+            return movies;
+          });
+      },
     },
   },
 });
 
-const RootQuery = new GraphQLObjectType({
-  name: "RootQuery",
-  fields: {
-    Movies: MovieQueryType,
-  },
-});
+// const RootQuery = new GraphQLObjectType({
+//   name: "RootQuery",
+//   fields: {
+//     Movies: {
+//       type: MovieQueryType,
+
+//     },
+//   },
+// });
 
 const schema = new GraphQLSchema({
-  query: RootQuery,
+  query: RootQueryType,
 });
 
 module.exports = schema;
