0c0002b03370f18f3fa8e46161c47f6f9e88c651
javin1102 <javinrio112@gmail.com>
2021-10-06 21:36:47 +0700

    implemented ssg for latest update movies

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,20 +2,54 @@ import LatestUpdate from "../components/Movies/LatestUpdate";
 import Header from "../components/Header/Header";
 import Nav from "../components/Nav/Nav";
 
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+  useQuery,
+} from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql",
   cache: new InMemoryCache(),
 });
 
-export default function Home() {
+export default function Home({ newMovies }) {
   return (
     <ApolloProvider client={client}>
       <Nav />
       <Header />
-      <LatestUpdate />
+      <LatestUpdate newMovies={newMovies} />
       <div className="min-h-[1000px]"></div>
     </ApolloProvider>
   );
+}
+
+const newMoviesGraphQL = async (page) => {
+  const movies = await client.query({
+    query: gql`
+      query {
+        newMovies(page: ${page}) {
+          title
+          poster_path
+        }
+      }
+    `,
+  });
+  return movies.data.newMovies;
+};
+
+export async function getStaticProps() {
+  const newMovies1 = await newMoviesGraphQL(1);
+  const newMovies2 = await newMoviesGraphQL(2);
+
+  const newMovies = [...newMovies1, ...newMovies2];
+
+  return {
+    props: {
+      newMovies,
+    },
+    revalidate: 10,
+  };
 }
