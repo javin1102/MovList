b747021d29d37ae24489004a3be5158e5a341087
javin1102 <javinrio112@gmail.com>
2021-10-06 21:37:39 +0700

    deleting comments

--- server/server.js
+++ server/server.js
@@ -17,23 +17,3 @@ app.use(
 );
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Listening on PORT 5000"));
-
-// const query = `{
-//     newMovies{
-//       id
-//       poster_path
-//       title
-//     }
-//   }`;
-
-// const d = async () => {
-//   await axios
-//     .post("http://localhost:4000/graphql", {
-//       query,
-//     })
-//     .then((res) => {
-//       const movies = res.data.data;
-//       console.log(movies);
-//     });
-// };
-// d();
