3bb1d1063f6deb3ef590b69128bb0f3e23b8d086
javin1102 <javinrio112@gmail.com>
2021-10-12 22:11:24 +0700

    fixing mov detail layout width

--- client/components/MovieDetail/MovieDetailLayout.jsx
+++ client/components/MovieDetail/MovieDetailLayout.jsx
@@ -1,6 +1,6 @@
 const MovieDetailLayout = (props) => {
   return (
-    <div className="flex flex-col w-full lg:w-3/4 px-4 md:px-12 mt-24 sm:mt-28 lg:mt-36 lg:px-24">
+    <div className="flex flex-col w-full lg:w-2/3 px-4 md:px-12 mt-24 sm:mt-28 lg:mt-36 lg:px-24">
       {props.children}
     </div>
   );
