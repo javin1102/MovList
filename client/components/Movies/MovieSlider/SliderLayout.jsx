1dad634cc2ba44aac08664f082318b2386ec0595
javin1102 <javinrio112@gmail.com>
2021-10-12 15:35:26 +0700

    fixing margin and spacing in movie slider

--- client/components/Movies/MovieSlider/SliderLayout.jsx
+++ client/components/Movies/MovieSlider/SliderLayout.jsx
@@ -1,6 +1,6 @@
 const SliderLayout = (props) => {
   return (
-    <div className="font-poppins pt-20 px-6 md:px-12 lg:px-24">
+    <div className="font-poppins pb-3 sm:pb-6 lg:pb-14 py-3 px-6 md:px-12 lg:px-24">
       {props.children}
     </div>
   );
