bc33c2fa5442a6d7f7e2dd364ab0ebab6052b796
javin1102 <javinrio112@gmail.com>
2021-10-13 00:13:14 +0700

    fixing add comment form margin

--- client/components/Comment/AddComment.jsx
+++ client/components/Comment/AddComment.jsx
@@ -5,11 +5,11 @@ const AddComment = () => {
   return (
     <div className="flex w-full mt-8 overflow-hidden">
       <CommentProfilePic />
-      <form className="w-full mt-2 text-xs md:text-sm ml-auto overflow-hidden">
+      <form className="w-full mt-2 text-xs md:text-sm ml-4 overflow-hidden">
         <textarea
           rows={1}
           placeholder="Add your review...."
-          className="resize-none border-b-[2px] h-[fit-content] pb-2  border-primary outline-none focus:outline-none w-full ml-5 font-normal"
+          className="resize-none border-b-[2px] h-[fit-content] pb-2 border-primary outline-none focus:outline-none w-full ml-5 font-normal"
         />
         <div className="w-[fit-content] font-poppins  ml-auto mt-3">
           <span className="text-black font-medium mr-3 md:mr-5 lg:mr-8 cursor-pointer active:text-gray">
