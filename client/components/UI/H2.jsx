2761e7c138bdad835038a4fdf8d4c04da7c7e773
javin1102 <javinrio112@gmail.com>
2021-10-14 13:39:34 +0700

    fixing className props bug

--- client/components/UI/H2.jsx
+++ client/components/UI/H2.jsx
@@ -1,7 +1,9 @@
 const H2 = (props) => {
   return (
     <h2
-      className={`text-sm font-bold md:text-base lg:text-2xl font-poppins ${props.className}`}
+      className={`text-sm font-bold md:text-base lg:text-2xl font-poppins ${
+        !!props.className && props.className
+      }`}
     >
       {props.children}
     </h2>
