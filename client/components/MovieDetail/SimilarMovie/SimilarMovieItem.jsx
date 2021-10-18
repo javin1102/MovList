1b1f07555f4f6f595cb0beade6761da5ee476154
javin1102 <javinrio112@gmail.com>
2021-10-18 10:57:55 +0700

    added Link

--- client/components/MovieDetail/SimilarMovie/SimilarMovieItem.jsx
+++ client/components/MovieDetail/SimilarMovie/SimilarMovieItem.jsx
@@ -2,23 +2,25 @@ import H3 from "../../UI/H3";
 import P from "../../UI/P";
 import SimilarMovieCard from "./SimilarMovieCard";
 import SimilarMovieRating from "./SimilarMovieRating";
-
+import Link from "next/link";
 const SimilarMovieItem = ({ movie }) => {
   return (
-    <div className="flex mb-6 last:mb-0 cursor-pointer h-full">
-      <SimilarMovieCard poster={movie.backdrop_path} />
-      <div className="flex flex-col justify-between ml-5">
-        <H3 className="line-clamp-1">{movie.title}</H3>
-        <div>
-          <P className="line-clamp-1">{movie.genre_name.join(", ")}</P>
-          <P>{movie.release_date}</P>
+    <Link href={`/movie/${movie.id}`}>
+      <a className="flex mb-6 last:mb-0 cursor-pointer h-full">
+        <SimilarMovieCard poster={movie.backdrop_path} />
+        <div className="flex flex-col justify-between ml-5">
+          <H3 className="line-clamp-1">{movie.title}</H3>
+          <div>
+            <P className="line-clamp-1">{movie.genre_name.join(", ")}</P>
+            <P>{movie.release_date}</P>
+          </div>
+          <SimilarMovieRating
+            average={movie.vote_average.toFixed(1)}
+            count={movie.vote_count}
+          />
         </div>
-        <SimilarMovieRating
-          average={movie.vote_average.toFixed(1)}
-          count={movie.vote_count}
-        />
-      </div>
-    </div>
+      </a>
+    </Link>
   );
 };
 
