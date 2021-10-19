23d5279fa118d4221c58e60c2f77d0b294951cde
javin1102 <javinrio112@gmail.com>
2021-10-19 11:56:55 +0700

    fixed mobile modal style

--- client/components/UI/Modal.jsx
+++ client/components/UI/Modal.jsx
@@ -22,14 +22,14 @@ export const Modal = () => {
             setIsClickOverlay={setIsClickOverlay}
             bgColor={`bg-[rgba(0,0,0,0.2)]`}
           />
-          <div className="fixed w-max top-1/2 left-1/2 bg-white rounded-lg flex flex-col justify-center -translate-x-1/2 -translate-y-1/2 font-poppins px-12 py-6 z-30">
+          <div className="fixed w-3/4 top-1/2 left-1/2 bg-white rounded-lg flex flex-col justify-center -translate-x-1/2 -translate-y-1/2 font-poppins px-6 md:px-12 py-6 z-30 lg:w-max">
             <h1 className="font-bold text-lg text-red-600">Delete Comment</h1>
             <p className="font-normal text-xs md:text-sm lg:text-base mt-3">
               Are you sure you want to delete the comment?
             </p>
             <div className="ml-auto mt-8 ">
               <span
-                className="text-black font-medium mr-3 md:mr-5 lg:mr-8 cursor-pointer active:text-gray"
+                className="text-black font-medium mr-3 md:mr-5 text-xs sm:text-sm md:text-base lg:mr-8 cursor-pointer active:text-gray"
                 onClick={() => dispatch(uiAction.setModal({ modal: false }))}
               >
                 Cancel
